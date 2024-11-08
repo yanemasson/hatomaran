edec420050e873aaa03af66639ca12ad20da1ec1
yanemasson <gluedboy@gmail.com>
2024-11-08 19:06:59 +0700

    Add dist (I think)

---
+++ dist/ProjectData.ts
@@ -0,0 +1,29 @@
+import {Project} from '../src/types/Project.ts'
+export const projects : Project[] =[
+    {
+        title: 'Title1',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum esse expedita ipsum obcaecati officia quo.' ,
+        bechanceLink: '/#',
+        images: [
+            {
+                url: 'public/images/pohuypank.png',
+                description: 'Lorem ipsum dolor.'
+            }
+        ]
+    },
+    {
+        title: 'Title2',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis distinctio excepturi, laboriosam quaerat quibusdam tempore.',
+        bechanceLink: '/#',
+        images: [
+            {
+                url: 'public/images/boing.jpg',
+                description: 'Lorem ipsum dolor.'
+            },
+            {
+                url: 'public/images/boingdemo.jpg',
+                description: 'bubeba'
+            }
+        ]
+    }
+]
